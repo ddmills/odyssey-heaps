fbfb7c08f8878a2f796ef744c28c4a48a5b2e39d
Dalton Mills <mills.dalton@gmail.com>
2021-12-11 10:06:13 -0600

    update deploy script

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env
 
 git checkout gh-pages
-# git reset --hard origin/master
 
-C:\\HaxeToolkit\\haxe\\haxe.exe --connect 6000 compile-js.hxml
+c:/HaxeToolkit/haxe/haxe.exe ./compile-js.hxml
 
-# delete everything on the directory
-# except the build folder
-# find * -maxdepth 0 -name 'build' -prune -o -exec rm -rf '{}' ';'
+find * -maxdepth 0 -name 'build' -prune -o -exec rm -rf '{}' ';'
 
-# move the build folder content
-# to the repository root
-# mv ./build/* .
+mv ./build/js/* .
 
-# git rm -rf --cache .
+git rm -rf --cache .
 git add .
 git commit -m "deploy"
 
